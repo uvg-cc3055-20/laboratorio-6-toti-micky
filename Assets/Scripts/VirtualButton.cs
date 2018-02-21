fb78e4e50609805026f75f6512fdc7e7ed4d90d9
sam17332 <32148906+sam17332@users.noreply.github.com>
2018-02-21 09:18:36 -0600

    lab6 terminado

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+	private VirtualButtonBehaviour virtualBtn;
+	public Animator birdAnim;
+
+	private void Start()
+	{
+		virtualBtn = GetComponent<VirtualButtonBehaviour>(); 
+		virtualBtn.RegisterEventHandler(this);
+	}
+
+	private void update()
+	{
+		
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+		birdAnim.SetTrigger("sing");
+	}
+	
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+		
+	}
+	
+}
