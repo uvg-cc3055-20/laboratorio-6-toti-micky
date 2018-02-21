9141139bdb8b657626dbe91229b80ad2e5097e85
Alhvi Balcarcel <abalcarcel@gmail.com>
2018-02-21 03:45:15 -0600

    se agregaron imagenes para target y script de bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    Animator anim;
+    AudioSource audios;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        audios = GetComponent<AudioSource>();
+	}
+
+
+    void ResetHopInt()
+    {
+        anim.SetInteger("hop", 0);
+    }
+
+    void PlaySong()
+    {
+        audios.Play();
+    }
+
+}
